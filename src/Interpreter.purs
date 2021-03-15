a48c4f3f14dbec3d12575b233072a217d60fa502
TWSiO <TWSiO@protonmail.com>
2021-03-14 19:09:06 -0700

    Fixing operators so we can compare precedences across libraries.

--- src/Interpreter.purs
+++ src/Interpreter.purs
@@ -8,8 +8,8 @@ import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype, unwrap)
 import Text.Parsing.StringParser.Expr as Op
-import Data.Tuple (Tuple(..))
-import Data.Array (concat)
+import Data.Tuple (Tuple(..), snd)
+import Data.Array (concat, fold)
 
 import Ast as AST
 import Parse (parse, infixOp, prefixOp, postfixOp)
@@ -23,8 +23,61 @@ data OpMeta
 -- Is Lambda right?
 data Operator = Operator Lambda OpMeta
 
--- TODO Turn this into a record type with ten levels of precedence. Maybe have a locked top and bottom precedence.
-type FullOperators = Array (Array Operator)
+newtype Operators = Operators
+    { first :: Array Operator
+    , second :: Array Operator
+    , third :: Array Operator
+    , fourth :: Array Operator
+    , fifth :: Array Operator
+    , sixth :: Array Operator
+    , seventh :: Array Operator
+    , eighth :: Array Operator
+    , ninth :: Array Operator
+    }
+
+-- TODO should probably check for duplicates and decide how order effects it.
+-- Or just add namespaced
+instance semigroupOperators :: Semigroup Operators where
+    append (Operators a) (Operators b) = Operators
+        { first: a.first <> b.first
+        , second: a.second <> b.second
+        , third: a.third <> b.third
+        , fourth: a.fourth <> b.fourth
+        , fifth: a.fifth <> b.fifth
+        , sixth: a.sixth <> b.sixth
+        , seventh: a.seventh <> b.seventh
+        , eighth: a.eighth <> b.eighth
+        , ninth: a.ninth <> b.ninth
+        }
+
+instance monoidOperators :: Monoid Operators where
+    mempty = emptyOperators
+
+emptyOperators :: Operators
+emptyOperators = Operators
+    { first: []
+    , second: []
+    , third: []
+    , fourth: []
+    , fifth: []
+    , sixth: []
+    , seventh: []
+    , eighth: []
+    , ninth: []
+    }
+
+toArrays :: Operators -> Array (Array Operator)
+toArrays (Operators ops) = 
+    [ ops.first
+    , ops.second
+    , ops.third
+    , ops.fourth
+    , ops.fifth
+    , ops.sixth
+    , ops.seventh
+    , ops.eighth
+    , ops.ninth
+    ]
 
 getExpr :: Operator -> Op.Operator AST.Expression
 getExpr (Operator _ opMeta) = case opMeta of
@@ -35,11 +88,11 @@ getExpr (Operator _ opMeta) = case opMeta of
 getFn :: Operator -> Lambda
 getFn (Operator fn _) = fn
 
-getOpTable :: FullOperators -> Op.OperatorTable AST.Expression
-getOpTable = map (map getExpr)
+toOpTable :: Operators -> Op.OperatorTable AST.Expression
+toOpTable ops = map (map getExpr) (toArrays ops)
 
-getFunctions :: FullOperators -> Map String Value
-getFunctions fullOps = fromFoldable (map toTup (concat fullOps))
+getFunctions :: Operators -> Map String Value
+getFunctions ops = fromFoldable (map toTup (concat (toArrays ops)))
     where
       toTup (Operator fn opMeta) = Tuple (getName opMeta) (FunctionVal fn)
       getName = case _ of
@@ -47,7 +100,7 @@ getFunctions fullOps = fromFoldable (map toTup (concat fullOps))
                     Prefix name -> name
                     Postfix name -> name
 
-type Library = Tuple Environment FullOperators
+type Library = Tuple Environment Operators
 
 -- Not sure how to handle operators and general defined stuff quite yet.
 interpret :: Array Library -> String -> Effect Unit
@@ -73,22 +126,16 @@ eval env expr = do
     --pure $ Left "Not implemented"
     pure $ Right (StringVal "foobar")
 
--- Just using this while I refactor operators again.
-tempOps :: Op.OperatorTable AST.Expression
-tempOps =
-    [ [ infixOp Op.AssocRight "*" ]
-    , [ prefixOp "&", postfixOp "!", prefixOp "-" ]
-    , [ infixOp Op.AssocRight "+" ]
-    , [ infixOp Op.AssocRight "==" ]
-    ]
-
 -- Temporarily just joins all the libraries.
 -- Clean up types
+-- This should be more complicated to namespace things properly and whatnot.
 importLibs :: Array Library -> String -> Tuple Environment (Op.OperatorTable AST.Expression)
-importLibs libs script = Tuple (Environment { values: unions (libToMap <$> libs) }) tempOps
+importLibs libs script = Tuple newEnv newOps
     where
       getMap (Environment x) = x
-      libToMap (Tuple (Environment a) ops) = unions [a.values, (getFunctions ops)]
+      libToMap (Tuple (Environment a) opsToMap) = unions [a.values, (getFunctions opsToMap)]
+      newEnv = Environment { values: unions (libToMap <$> libs) }
+      newOps = toOpTable (fold (snd <$> libs))
 
 -- Should maybe just be Map String Value
 newtype Environment = Environment
