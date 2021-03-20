f80b0f88bee4891542e9e26119a8e5ff8e568df7
TWSiO <TWSiO@protonmail.com>
2021-03-19 17:13:46 -0700

    Adding comment about possible needed change.

--- src/Symbol.purs
+++ src/Symbol.purs
@@ -10,6 +10,7 @@ instance showSymbol :: Show Symbol where
 instance eqSymbol :: Eq Symbol where
     eq a b = eqCompare a b
 
+-- Should this have Effect or something? It's not exactly pure. Or is it?
 foreign import symbol :: String -> Symbol
 
 foreign import eqCompare :: Symbol -> Symbol -> Boolean
